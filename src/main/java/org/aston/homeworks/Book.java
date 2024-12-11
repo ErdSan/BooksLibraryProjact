06a9eb04ed1137fc294d8193a0a60ef7b7297aa8
ErdSan <sanzhi.erdyneev@gmail.com>
2024-12-12 00:53:47 +0800

    Изменил тип поля на Integer и убрал лишние геттеры и сеттеры

--- src/main/java/org/aston/homeworks/Book.java
+++ src/main/java/org/aston/homeworks/Book.java
@@ -3,31 +3,11 @@ package org.aston.homeworks;
 public class Book {
     private String title;
     private String author;
-    private int year;
+    private Integer year;
     private boolean isAvailable = true;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
     }
 
     public boolean isAvailable() {
